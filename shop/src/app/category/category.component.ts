c38d9945395dc05bd63a5de6467394e5a1137393
tugba-koc <71395416+tugba-koc@users.noreply.github.com>
2021-01-03 15:28:13 +0300

    shop/angular

---
+++ shop/src/app/category/category.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Category } from "./category"
+
+@Component({
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.css'],
+})
+export class CategoryComponent implements OnInit {
+  constructor() {}
+
+  title = 'Kategoriler';
+
+  categories: Category[] = [
+    { id: 1, name: 'Elektronik' },
+    { id: 1, name: 'Bilgisayar' },
+    { id: 1, name: 'İletişim' },
+    { id: 1, name: 'İçecek' },
+    { id: 1, name: 'Müzik' },
+  ];
+
+  ngOnInit(): void {}
+}
